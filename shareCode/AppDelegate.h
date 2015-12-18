ec54e69fc6a1fa8775c0365162188370ecaccc83
AppriaTT <yangzhwork@126.com>
2015-12-18 16:30:12 +0800

    https://github.com/yangzh1992/shareCode.git
    https://github.com/yangzh1992/shareCode.git

---
+++ shareCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shareCode
+//
+//  Created by qianfeng on 15/12/17.
+//  Copyright (c) 2015年 叶无道. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shareCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  shareCode
+//
+//  Created by qianfeng on 15/12/17.
+//  Copyright (c) 2015年 叶无道. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
